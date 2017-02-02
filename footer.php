5d2059a90fb333bcea7b0e8e16440813df5b2c6a
InstaGib111 <achard.christopher@gmail.com>
2017-02-02 12:29:15 +0100

    footer presque fini manque plus que les liens clicable

--- footer.php
+++ footer.php
@@ -1,29 +1,35 @@
 <footer id="foot" class="row">
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="contacte">
-		<div class="foodi">
-			<span>Contacter le vendeur</span>
+		<div class="foodi col-xs-12">
+			<span class="col-xs-8">Contacter le vendeur</span>
+			<img src="<?php echo $BASE_URL ?>/cont/img/enveloppe.png" class="fooImg col-xs-4" alt="Enveloppe" id="fooEnv" />
 		</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="paiement">
-		<div class="foodi">
-			<span>Paiement sécurisé avec PayPal</span>
+		<div class="foodi col-xs-12">
+			<span class="col-xs-8">Paiement sécurisé avec PayPal</span>
+			<img src="<?php echo $BASE_URL ?>/cont/img/Payment.png" class="fooImg col-xs-4" alt="paypal" id="fooPaypal" />
 		</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="adminBO">
-		<div class="foodi">
-			<span>Accèder au panel administration</span>
+		<div class="foodi col-xs-12">
+			<span class="col-xs-8">Accèder au panel administrateur</span>
+			<img src="<?php echo $BASE_URL ?>/cont/img/Gear" class="fooImg col-xs-4" alt="Camion" id="fooGear" />
 		</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="livraison">
-		<div class="foodi">
-			<span>Livraison chrono dans toute la france</span>
+		<div class="foodi col-xs-12">
+			<span class="col-xs-8">Livraison chrono dans toute la france</span>
+			<img src="<?php echo $BASE_URL ?>/cont/img/camion" class="fooImg col-xs-4" alt="Camion" id="fooCamion" />
 		</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="reseaux">
-		<div class="foodi">
-			<span>Suivez-nous sur les réseaux sociaux</span>
+		<div class="foodi col-xs-12">
+			<span class="col-xs-8">Suivez-nous sur les réseaux sociaux</span>
+			<a id="foofb" target="_blank" href="https://www.facebook.com/La-Fromagerie-1517997551795188/?fref=ts">
+				<img src="<?php echo $BASE_URL ?>cont/img/fb.png" class="fooImg col-xs-4" alt="fB">
+			</a>
 		</div>
 	</div>
-
 </footer>
 </div>
