56b56f678dcc613fe6a674b5a245a589f1580f0a
InstaGib111 <achard.christopher@gmail.com>
2017-02-02 11:36:05 +0100

    changement sur le footer (pas fini)

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
-<footer>
-	
-	
+<footer id="foot" class="row">
+	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="contacte">
+		<div class="foodi">
+			<span>Contacter le vendeur</span>
+		</div>
+	</div>
+	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="paiement">
+		<div class="foodi">
+			<span>Paiement sécurisé avec PayPal</span>
+		</div>
+	</div>
+	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="adminBO">
+		<div class="foodi">
+			<span>Accèder au panel administration</span>
+		</div>
+	</div>
+	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="livraison">
+		<div class="foodi">
+			<span>Livraison chrono dans toute la france</span>
+		</div>
+	</div>
+	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-push-1 col-lg-2" id="reseaux">
+		<div class="foodi">
+			<span>Suivez-nous sur les réseaux sociaux</span>
+		</div>
+	</div>
+
 </footer>
 </div>
